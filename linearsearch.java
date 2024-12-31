539940dc4b95ed1f68ff5f33843dd6eb5c3182b2
Rabiya Tahseen <166829575+rabiyatahseen@users.noreply.github.com>
2024-12-31 11:13:16 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -1,39 +1,33 @@
 import java.util.Scanner;
 
-public class linearsearch {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        System.out.print("Enter the element to search: ");
-        int x = sc.nextInt();
-
-        int result = linearSearch(arr, x);
-        if (result == -1) {
-            System.out.println("Element not found");
-        } else {
-            System.out.println("Element found at index " + result);
-        }
-    }
-
-    public static int linearSearch(int[] arr, int x) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == x) {
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
                 return i;
             }
         }
         return -1;
     }
-}
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);r
+        System.out.println("Enter number of elements:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Enter the element to search for:");
+        int key = scanner.nextInt();
+        int result = linearSearch(arr, key); 
+        if (result == -1) {
+            System.out.println("Element not found.");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+
+        scanner.close();
+    }
+}
