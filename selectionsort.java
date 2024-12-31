7ebadbe8eb9756fc22ce2b6524daf51caf3dd153
Rabiya Tahseen <166829575+rabiyatahseen@users.noreply.github.com>
2024-12-31 11:12:36 +0530

    Update selectionsort.java

--- selectionsort.java
+++ selectionsort.java
@@ -1,44 +1,34 @@
 import java.util.Scanner;
-
-public class selectionsort {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        selectionSort(arr);
-
-        System.out.println("Sorted Array:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void selectionSort(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            // Swap arr[i] and arr[minIndex]
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
+import java.util.Arrays;
+public class selectionsort{
+	public static void selectionsort(int[] arr){
+		int n=arr.length;
+		for(int i=0;i<n;i++){
+			int minindex=i;
+		for(int j=i+1;j<n;j++){
+			if(arr[j]<arr[minindex]){
+				minindex=j;
+				}
+			}
+			int temp=arr[minindex];
+			arr[minindex]=arr[i];
+			arr[i]=temp;
+			}
+		} 
+		public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the number of elements:");
+		int n = scanner.nextInt();
+		int[] arr=new int[n];
+		System.out.println("enter the elements:");
+		for(int i=0;i<n;i++){
+			arr[i] = scanner.nextInt();
+		}
+		selectionsort(arr);
+		System.out.println("sorted array:");
+		for(int num : arr){
+			System.out.print(num+" ");
+		}
+	}
 }
 
