435026b867c85048ba2bc216675a371bb5057ed0
Rabiya Tahseen <166829575+rabiyatahseen@users.noreply.github.com>
2024-12-31 10:40:18 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,39 +1,31 @@
 import java.util.Scanner;
-
-public class bubblesort {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        bubbleSort(arr);
-
-        System.out.println("Sorted Array:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
+public class bubblesort{
+	public static void main(String[] args){
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the number of elements:");
+		int n = scanner.nextInt();
+		int[] arr=new int[n];
+		System.out.println("enter the elements:");
+		for(int i=0;i<n;i++){
+			arr[i] = scanner.nextInt();
+		}
+		bubblesort(arr);
+		System.out.println("sorted array:");
+		for(int num : arr){
+			System.out.print(num+" ");
+		}
+	}
+	public static void bubblesort(int[] arr){
+		int n = arr.length;
+		for(int i=0;i<n-1;i++){
+			for(int j=0;j<n-1-i;j++){
+				if(arr[j]>arr[j+1]){
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					}
+				}
+			}
+		}
 }
+		
