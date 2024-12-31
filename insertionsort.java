5ff05f042a6809e02d547cc5039373e5cd5a1e2c
Rabiya Tahseen <166829575+rabiyatahseen@users.noreply.github.com>
2024-12-31 10:40:58 +0530

    Update insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,37 +1,10 @@
 import java.util.Scanner;
-
 public class insertionsort {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number of elements: ");
-        int n = sc.nextInt();
-
-        int[] arr = new int[n];
-
-        System.out.println("Enter the elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        insertionSort(arr);
-
-        System.out.println("Sorted Array:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void insertionSort(int[] arr) {
+    public static void insertionsort(int[] arr) {
         int n = arr.length;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
             int j = i - 1;
-
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -39,5 +12,24 @@ public class insertionsort {
             arr[j + 1] = key;
         }
     }
-}
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        // Read the elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        insertionsort(arr);
+
+        // Print the sorted array
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
